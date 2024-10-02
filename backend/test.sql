27cc92fb3ffdb3818e5d99b0b56cb0a62ca45549
DharmaMakkena <dharmamakkena06@gmail.com>
2024-10-02 14:22:42 -0400

    Update test.sql

--- backend/test.sql
+++ backend/test.sql
@@ -27,21 +27,21 @@ SET time_zone = "+00:00";
 -- Table structure for table `books`
 --
 
-CREATE TABLE `books` (
+/*CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `title` varchar(300) NOT NULL,
   `desc` varchar(500) NOT NULL,
   `price` float NOT NULL,
   `cover` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;*/
 
 --
 -- Dumping data for table `books`
 --
 
 INSERT INTO `books` (`id`, `title`, `desc`, `price`, `cover`) VALUES
-(1, 'Gamer of throne', 'this is an amazing book to read when you are free ', 2343.2, 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
-(2, 'Fire folks', 'fire folks is ming blowing book to read it will blow your mind', 2342.3, 'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80');
+(4, 'Gamer of throne by Dharma', 'this is an amazing book to read when you are free ', 2343.2, 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
+(5, 'Fire folks By Dharma', 'fire folks is ming blowing book to read it will blow your mind', 2342.3, 'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80');
 --
 -- Indexes for dumped tables
 --
@@ -49,8 +49,8 @@ INSERT INTO `books` (`id`, `title`, `desc`, `price`, `cover`) VALUES
 --
 -- Indexes for table `books`
 --
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+/*ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);*/
 
 --
 -- AUTO_INCREMENT for dumped tables
